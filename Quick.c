6afe0fece5208333a432522ca60b065b021296fd
mrrandom-afk <mrrandom8180@gmail.com>
2026-08-20 22:02:20 +0530

    quick.c

--- Quick.c
+++ Quick.c
@@ -1,53 +1,35 @@
 #include <stdio.h>
-void display(int a[], int n)
-{
-for(int i=0;i<n;i++)
-printf("%d ",a[i]);
-printf("\n");
-}
-int partition(int a[], int lb, int ub)
-{
-int pivot=a[lb];
-int start=lb,end=ub,temp;
-while(start<end)
-{
-while(a[start]<=pivot && start<ub)
-start++;
-while(a[end]>pivot)
-end--;
-if(start<end)
-{
-temp=a[start];
-a[start]=a[end];
-a[end]=temp;
-}
-}
-temp=a[lb];
-a[lb]=a[end];
-a[end]=temp;
-return end;
-}
-void quicksort(int a[], int lb, int ub)
-{
 
-if(lb<ub)
-{
-int loc=partition(a,lb,ub);
-quicksort(a,lb,loc-1);
-quicksort(a,loc+1,ub);
+int partition(int a[], int lb, int ub) {
+    int p = a[lb], s = lb, e = ub, t;
+    while (s < e) {
+        while (a[s] <= p && s < ub) s++;
+        while (a[e] > p) e--;
+        if (s < e) { t = a[s]; a[s] = a[e]; a[e] = t; }
+    }
+    t = a[lb]; a[lb] = a[e]; a[e] = t;
+    return e;
 }
+
+void quicksort(int a[], int lb, int ub) {
+    if (lb < ub) {
+        int loc = partition(a, lb, ub);
+        quicksort(a, lb, loc - 1);
+        quicksort(a, loc + 1, ub);
+    }
 }
-int main()
-{
-int n,a[50];
-printf("Enter number of elements: ");
-scanf("%d",&n);
-printf("Enter elements:\n");
-for(int i=0;i<n;i++)
-scanf("%d",&a[i]);
-quicksort(a,0,n-1);
-printf("Sorted Array:\n");
-for(int i=0;i<n;i++)
-printf("%d ",a[i]);
-return 0;
+
+int main() {
+    int n, a[50];
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    quicksort(a, 0, n - 1);
+    printf("Sorted Array:\n");
+    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
 }
+
+
+
