6c4f9bf0978d7b7bb2963de2a26b5127948681cc
mrrandom-afk <mrrandom8180@gmail.com>
2026-08-20 21:53:39 +0530

    inser.c

--- inser.c
+++ inser.c
@@ -1,42 +1,26 @@
 #include <stdio.h>
-int main()
-{
-int a[100], n, i, j, temp, k;
-printf("Enter number of elements: ");
-scanf("%d", &n);
-printf("Enter elements:\n");
+int main() {
+    int a[100], n, temp, j;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
 
-for(i = 0; i < n; i++)
-{
-scanf("%d", &a[i]);
+    printf("\nInitial Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+
+    for (int i = 1; i < n; i++) {
+        temp = a[i];
+        for (j = i - 1; j >= 0 && a[j] > temp; j--)
+            a[j + 1] = a[j];
+        a[j + 1] = temp;
+
+        printf("\nPass %d: ", i);
+        for (int k = 0; k < n; k++) printf("%d ", a[k]);
+    }
+
+    printf("\n\nSorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", a[i]);
+    return 0;
 }
-printf("\nInitial Array: ");
-for(i = 0; i < n; i++)
-{
-printf("%d ", a[i]);
-}
-printf("\n");
-for(i = 1; i < n; i++)
-{
-temp = a[i];
-j = i - 1;
-while(j >= 0 && a[j] > temp)
-{
-a[j + 1] = a[j];
-j--;
-}
-a[j + 1] = temp;
-printf("Pass %d: ", i);
-for(k = 0; k < n; k++)
-{
-printf("%d ", a[k]);
-}
-printf("\n");
-}
-printf("\nSorted Array: ");
-for(i = 0; i < n; i++)
-{
-printf("%d ", a[i]);
-}
-return 0;
-}
+
